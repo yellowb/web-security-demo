e8fb02cc5eee5208b73e1e5cfac1284feee348da
Yellow Huang <yellow.huang@oocl.com>
2017-04-03 15:15:19 +0800

    complete the login logic

--- routes/login.js
+++ routes/login.js
@@ -3,9 +3,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login.ejs');
+var users = [{
+    userid: 'yellow',
+    password: 'yellow'
+}, {
+    userid: 'tom',
+    password: 'tom'
+}];
+
+function checkUserPwd(userid, password) {
+    for(var i in users) {
+        if(users[i].userid === userid && users[i].password === password) {
+            return true;
+        }
+    }
+    return false;
+}
+
+router.get('/', function (req, res, next) {
+    res.render('login.ejs');
+});
+
+router.post('/check', function (req, res, next) {
+
+    console.log('Check: ' + checkUserPwd(req.body.userid, req.body.password));
+
+    if(checkUserPwd(req.body.userid, req.body.password)) {
+        res.redirect('/comments/list');
+    }
+    else {
+        res.redirect('/login');
+    }
 });
 
 module.exports = router;
